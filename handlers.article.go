8182ddebb3167eddd5a00cf2a2b54f21eafce508
YoshidaKazuki <yoshidakazuki@team-lab.com>
2018-10-12 11:13:38 +0900

    個別記事ページ機能を追加しJSON,XMLにも対応

--- handlers.article.go
+++ handlers.article.go
@@ -2,19 +2,33 @@ package main
 
 import (
   "net/http"
+  "strconv"
+
   "github.com/gin-gonic/gin"
 )
 
 func showIndexPage(c *gin.Context) {
   articles := getAllArticles()
-
-  c.HTML(
-    http.StatusOK,
-    "index.html",
-    gin.H{
+  render(c,gin.H{
       "title": "Home Page",
-      "payload": articles,
-    },
-  )
+      "payload": articles},"index.html")
 }
 
+func getArticle(c *gin.Context) {
+  if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
+    if article, err := getArticleByID(articleID); err == nil {
+      c.HTML(
+        http.StatusOK,
+        "article.html",
+        gin.H{
+          "title": article.Title,
+          "payload": article,
+        },
+      )
+    } else {
+      c.AbortWithError(http.StatusNotFound, err)
+    }
+  } else {
+    c.AbortWithStatus(http.StatusNotFound)
+  }
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "net/http"
   "github.com/gin-gonic/gin"
 )
 
@@ -11,4 +12,15 @@ func main() {
   router.LoadHTMLGlob("templates/*")
   initializeRoutes()
   router.Run()
+}
+
+func render(c *gin.Context, data gin.H, templateName string) {
+  switch c.Request.Header.Get("Accept") {
+  case "application/json":
+    c.JSON(http.StatusOK, data["payload"])
+  case "application/xml":
+    c.XML(http.StatusOK, data["payload"])
+  default:
+    c.HTML(http.StatusOK, templateName, data)
+  }
 }
